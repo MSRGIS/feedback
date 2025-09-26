80eb2f4612b59f5560972ed6891c2487d2a2ef0d
shneor770 <57337950+shneor770@users.noreply.github.com>
2025-09-26 08:37:31 +0300

    Update firebase-config.js

--- netlify/functions/firebase-config.js
+++ netlify/functions/firebase-config.js
@@ -16,6 +16,7 @@ exports.handler = async function(event, context) {
   const firebaseConfig = {
     apiKey: process.env.VITE_API_KEY,
     authDomain: process.env.VITE_AUTH_DOMAIN,
+    databaseURL: process.env.VITE_DATABASE_URL,
     projectId: process.env.VITE_PROJECT_ID,
     storageBucket: process.env.VITE_STORAGE_BUCKET,
     messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
@@ -27,3 +28,4 @@ exports.handler = async function(event, context) {
     body: JSON.stringify(firebaseConfig),
   };
 };
+
